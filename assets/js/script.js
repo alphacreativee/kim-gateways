4f2f74eb4374ba6343816818092bc1d5a2d7adf3
HuuBang11 <huubang0312@gmail.com>
2025-01-17 14:38:57 +0700

    update homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,64 @@ $ = jQuery;
 $(document).ready(function(){
     header();
     createFilter();
+    swiperBanner();
 })
 
 gsap.registerPlugin(ScrollTrigger);
-
 window.addEventListener("load", () => {
   ScrollTrigger.refresh();
 });
+
+function swiperBanner() {
+  if ($(".banner-hero").length) {
+    var interleaveOffset = 0.9;
+
+    var swiperBanner = new Swiper(".swiper-banner", {
+      loop: true,
+      speed: 1500,
+      grabCursor: true,
+      watchSlidesProgress: true,
+      mousewheelControl: true,
+      keyboardControl: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: true,
+      },
+      on: {
+        progress: function (swiper) {
+          swiper.slides.forEach(function (slide) {
+            var slideProgress = slide.progress || 0;
+            var innerOffset = swiper.width * interleaveOffset;
+            var innerTranslate = slideProgress * innerOffset;
+            // Kiểm tra nếu innerTranslate không phải là NaN
+            if (!isNaN(innerTranslate)) {
+              var slideInner = slide.querySelector(".slide-banner");
+              if (slideInner) {
+                slideInner.style.transform =
+                  "translate3d(" + innerTranslate + "px, 0, 0)";
+              }
+            }
+          });
+        },
+        touchStart: function (swiper) {
+          swiper.slides.forEach(function (slide) {
+            slide.style.transition = "";
+          });
+        },
+        setTransition: function (swiper, speed) {
+          var easing = "cubic-bezier(0.25, 0.1, 0.25, 1)";
+          swiper.slides.forEach(function (slide) {
+            slide.style.transition = speed + "ms " + easing;
+            var slideInner = slide.querySelector(".slide-banner");
+            if (slideInner) {
+              slideInner.style.transition = speed + "ms " + easing;
+            }
+          });
+        },
+      },
+    });
+  }
+}
 
 function header(){
   ScrollTrigger.create({
