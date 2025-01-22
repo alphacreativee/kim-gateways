de154ac3bb11f7071cf9221c3dc059b82b923076
HuuBang11 <huubang0312@gmail.com>
2025-01-22 09:17:54 +0700

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
   contactForm();
   counterOnScroll();
   coreValue();
+  customDropdown();
 });
 
 gsap.registerPlugin(ScrollTrigger);
@@ -77,6 +78,21 @@ function header() {
       self.progress === 0 ? $("header").removeClass("header--scroll") : "";
     },
   });
+}
+
+function togglePopupHeader(event){
+  event.preventDefault();
+
+  let btnHamburger = $("#header .navbar-toggler");
+  btnHamburger.toggleClass("is-active");
+
+  $("body").toggleClass("overflow-hidden");
+
+  let headerBackdrop = $("#header .header__backdrop");
+  headerBackdrop.toggleClass("open");
+
+  let menuMobile = $("#header .header__menu");
+  menuMobile.toggleClass("show");
 }
 
 function createFilter() {
@@ -267,3 +283,66 @@ var swiper2 = new Swiper(".mySwiper2", {
   freeMode: true,
   speed: 10000,
 });
+
+function customDropdown() {
+  const $dropdowns = $(".dropdown-custom");
+
+  $dropdowns.each(function () {
+    const $dropdown = $(this);
+    const $btnDropdown = $dropdown.find(".dropdown-custom__btn");
+    const $dropdownMenu = $dropdown.find(".dropdown-custom__menu");
+    const $dropdownItems = $dropdown.find(".dropdown-custom__item");
+    const $textDropdown = $dropdown.find(".dropdown-custom__text");
+
+    $btnDropdown.on("click", function (e) {
+      e.stopPropagation();
+      closeAllDropdowns($dropdown);
+      $dropdownMenu.toggleClass("dropdown--active");
+      $btnDropdown.toggleClass("--active");
+    });
+
+    $(document).on("click", function () {
+      closeAllDropdowns();
+    });
+
+    $dropdownItems.on("click", function (e) {
+      e.stopPropagation();
+      const $item = $(this);
+      let tmpText = $textDropdown.text();
+      const tmpImgSrc = $textDropdown.find("img").attr("src"); // Get the current image src if present
+      const $img = $item.find("img"); // Check if the clicked item contains an img
+
+      // Swap text content
+      $textDropdown.text($item.text());
+
+      // If the item has an image, swap the img src
+      if ($img.length) {
+        $textDropdown.html($item.html()); // Swap the entire HTML, including the img
+
+        if ($item.hasClass("language__item")) {
+          tmpText = `<span>${tmpText}</span>`;
+        }
+
+        $item.html(
+          `${tmpImgSrc ? `<img src="${tmpImgSrc}" />` : ""} ${tmpText}`
+        ); // Swap img and text back to the item
+      } else if ($item.hasClass("language__item")) {
+        $item.text(tmpText);
+      }
+
+      closeAllDropdowns();
+    });
+
+    function closeAllDropdowns(exception) {
+      $(".dropdown-custom__btn").removeClass("active");
+      $dropdowns.each(function () {
+        const $menu = $(this).find(".dropdown-custom__menu");
+        const $ic = $(this).find(".dropdown-custom__btn");
+        if (!exception || !$(this).is(exception)) {
+          $menu.removeClass("dropdown--active");
+          $ic.removeClass("--active");
+        }
+      });
+    }
+  });
+}
